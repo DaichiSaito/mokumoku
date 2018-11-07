62c9fdc3fa2061849b9d4a774fcf5fc745601845
ArtefactAir <artefactapple@gmail.com>
2018-11-07 16:39:58 +0900

    日本語化の設定

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,10 @@ module MOKUMOKU
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
+    # 日本語化の設定
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = %i[ja]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
