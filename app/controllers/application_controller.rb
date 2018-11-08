8c344f14bf0cb7d1d970a9504c6eb557f9d37463
ArtefactAir <artefactapple@gmail.com>
2018-11-08 11:44:52 +0900

    アプリケーションコントローラー、ビューの実装 ・ログイン制御の追加 ・レイアウトファイルの切り替え ・ヘッダー、フッターの表示の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  layout :select_layout
+
+  private
+
+  def select_layout
+    return 'before_login' if current_user.blank?
+
+    'application'
+  end
+
+  def not_authenticated
+    redirect_to login_path, danger: t('flash.common.not_authenticated')
+  end
 end
