3fe9503b4c7ddfc253b18b7bf29476e5e50cbf22
ArtefactAir <artefactapple@gmail.com>
2018-11-09 16:37:10 +0900

    Twitter 認証のためのマイグレーションの追加
    rails g sorcery:install external --only-submodules
    
    公式：https://github.com/NoamB/sorcery/wiki/External

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_030950) do
+ActiveRecord::Schema.define(version: 2018_11_09_073212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,15 @@ ActiveRecord::Schema.define(version: 2018_11_08_030950) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "authentications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
   end
 
   create_table "favorite_areas", force: :cascade do |t|
