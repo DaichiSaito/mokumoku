a1f9940962f48988fc37b2dd844ced4efcd0165b
ArtefactAir <artefactapple@gmail.com>
2018-11-09 13:49:58 +0900

    不要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   resources :tops, only: :index
   resources :users, only: %i[new create]
-  resources :user_sessions, only: %i[new create destroy]
 
   root 'tops#index'
 end
