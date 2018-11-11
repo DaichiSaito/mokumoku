03b5b425b7b3de072328b810364ae28cdadb8e63
ArtefactAir <artefactapple@gmail.com>
2018-11-11 11:07:01 +0900

    OAuth認証コントローラーの仮実装とファイルパスの変更

---
+++ app/controllers/users/oauths_controller.rb
@@ -0,0 +1,29 @@
+class Users::OauthsController < GeneralController
+  skip_before_action :require_login
+
+  # sends the user on a trip to the provider,
+  # and after authorizing there back to the callback url.
+  def oauth
+    login_at(params[:provider])
+  end
+
+  def callback
+    provider = :twitter
+
+    @user = login_from(provider)
+    if @user.present?
+      redirect_to root_path
+    else
+      begin
+        @user = create_from(provider)
+
+        reset_session # protect from session fixation attack
+        auto_login(@user)
+        redirect_to root_path
+      rescue StandardError => e
+        logger.error(e)
+        redirect_to root_path
+      end
+    end
+  end
+end
