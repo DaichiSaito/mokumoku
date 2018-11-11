f5ca04c80d67e97d66bbf59fb5b6109170239bae
ArtefactAir <artefactapple@gmail.com>
2018-11-11 18:45:04 +0900

    Twitter 認証処理の修正 ・認証情報取得処理を create_and_validate_from() で行うように修正 　Twitter から取得したパラメータをセッションに保存して User のインスタンスを生成する 　（追加でパラメータを入力させたい場合に推奨されているため） ・オートログインの追加

--- app/controllers/users/oauths_controller.rb
+++ app/controllers/users/oauths_controller.rb
@@ -1,5 +1,6 @@
 class Users::OauthsController < GeneralController
   skip_before_action :require_login
+  before_action :set_provider_name, only: %i[callback new]
 
   # sends the user on a trip to the provider,
   # and after authorizing there back to the callback url.
@@ -8,31 +9,34 @@ class Users::OauthsController < GeneralController
   end
 
   def callback
-    provider = :twitter
-
     return denied_app_collaborate if params[:denied].present?
 
     begin
-      @user = login_from(provider)
+      @user = login_from(@provider_name)
       return redirect_to root_path if @user.present?
     rescue OAuth::Unauthorized => e
       logger.error(e)
       return redirect_to root_path
     end
 
-    setup_user_instance
-    reset_session # protect from session fixation attack
+    setup_user_instance @provider_name
     render :new
   end
 
   def new
-    setup_user_instance
+    setup_user_instance @provider_name
   end
 
   def create
     @user = User.new(user_params)
+    # new アクションで実行した create_and_validate_from() の中でセッションに保存したパラメータを取得し、認証クラスの生成に用いる
+    # （add_provider_to_user() で生成する方法は「401 Authorization Required」が解決出来なかった）
+    @user.authentications.build(session[:incomplete_user]['provider'])
+
     if @user.save
-      redirect_to login_url, success: 'ユーザーを作成しました'
+      reset_session # protect from session fixation attack
+      auto_login(@user)
+      redirect_to root_url, success: 'ユーザーを作成しました'
     else
       flash.now[:danger] = 'ユーザーが作成出来ませんでした'
       render :new
@@ -45,16 +49,22 @@ class Users::OauthsController < GeneralController
     params.require(:user).permit(:name, :email, :profile, :avatar, :password, :password_confirmation, favorite_areas_attributes: [:area_id])
   end
 
+  # アプリ連携を拒否
   def denied_app_collaborate
     logger.error('authenticate denied')
     return redirect_to new_user_url
   end
 
-  def setup_user_instance
-    provider = :twitter
-    # see create_from() https://github.com/NoamB/sorcery/blob/master/lib/sorcery/controller/submodules/external.rb
-    sorcery_fetch_user_hash provider
-    @user = User.new(user_attrs(@provider.user_info_mapping, @user_hash))
+  def setup_user_instance(provider_name)
+    # see https://github.com/NoamB/sorcery/blob/master/lib/sorcery/controller/submodules/external.rb
+    @user = create_and_validate_from provider_name
     @user.favorite_areas.build
+  end
+
+  # TODO: Twitter で決め打ち指定
+  # Twitter 認証のコールバックでクエリ文字を含めることが出来ず、
+  # oauth/request_tokenリクエストが必要になるようなので、ひとまず決め打ち指定で実装
+  def set_provider_name
+    @provider_name = :twitter
   end
 end
