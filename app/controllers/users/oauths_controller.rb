f6e0445a3ac12c320185fbcf10a9e7b2cac39d5e
ArtefactAir <artefactapple@gmail.com>
2018-11-11 11:44:14 +0900

    Twitter 認証コールバック画面でリロードすると401エラーになる対応 login_from() で401エラーになるためリダイレクト処理を追加

--- app/controllers/users/oauths_controller.rb
+++ app/controllers/users/oauths_controller.rb
@@ -15,16 +15,19 @@ class Users::OauthsController < GeneralController
       return redirect_to root_path
     end
 
-    @user = login_from(provider)
-    if @user.present?
-      redirect_to root_path
-    else
-      # see create_from() https://github.com/NoamB/sorcery/blob/master/lib/sorcery/controller/submodules/external.rb
-      sorcery_fetch_user_hash provider
-      @user = User.new(user_attrs(@provider.user_info_mapping, @user_hash))
-      @user.favorite_areas.build
-
-      reset_session # protect from session fixation attack
+    begin
+      @user = login_from(provider)
+      return redirect_to root_path if @user.present?
+    rescue OAuth::Unauthorized => e
+      logger.error(e)
+      return redirect_to root_path
     end
+
+    # see create_from() https://github.com/NoamB/sorcery/blob/master/lib/sorcery/controller/submodules/external.rb
+    sorcery_fetch_user_hash provider
+    @user = User.new(user_attrs(@provider.user_info_mapping, @user_hash))
+    @user.favorite_areas.build
+
+    reset_session # protect from session fixation attack
   end
 end
