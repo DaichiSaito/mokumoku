47c1452b9564fb8c8e536c82ce2455a826741ef6
ArtefactAir <artefactapple@gmail.com>
2018-11-14 17:44:22 +0900

    お気に入りエリア、エリアのアソシエーションを追加

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,3 +1,5 @@
 class Area < ApplicationRecord
   has_many :mokumokus
+  has_many :favorite_areas
+  has_many :users, through: :favorite_areas
 end
--- app/models/favorite_area.rb
+++ app/models/favorite_area.rb
@@ -1,4 +1,7 @@
 class FavoriteArea < ApplicationRecord
+  belongs_to :user
+  belongs_to :area
+
   def area_name
     Area.find(area_id).name
   end
