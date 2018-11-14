d86fedefd7c6ff09a82e1659ff70931d599008cc
DaichiSaito <h1a_ble@yahoo.co.jp>
2018-11-14 17:04:29 +0900

    be rails g model notification body:text notifiable:references{polymorphic} user:references read:boolean

---
+++ db/migrate/20181114080416_create_notifications.rb
@@ -0,0 +1,12 @@
+class CreateNotifications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :notifications do |t|
+      t.text :body
+      t.references :notifiable, polymorphic: true
+      t.references :user, foreign_key: true
+      t.boolean :read
+
+      t.timestamps
+    end
+  end
+end
