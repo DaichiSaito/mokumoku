c84688f17044073ed1e7798d04aad70326e5bfa5
DaichiSaito <h1a_ble@yahoo.co.jp>
2018-11-15 18:01:27 +0900

    メール送信機能

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,18 @@
 class NotificationMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def send_favorite_areas_user(user, mokumoku)
+    @mokumoku = mokumoku
+    mail(to: user.email, subject: 'あなたのお気に入りエリアのもくもくが投稿されました。')
+  end
+
+  def send_mokumoku_owner(user, mokumoku)
+    @mokumoku = mokumoku
+    mail(to: user.email, subject: 'あなたのもくもくにコメントがありました。')
+  end
+
+  def send_mokumoku_participants(user, mokumoku)
+    @mokumoku = mokumoku
+    mail(to: user.email, subject: 'あなたの参加予定のもくもくにコメントがありました。')
+  end
 end
