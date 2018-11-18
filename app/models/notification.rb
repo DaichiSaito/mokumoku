7a7e1fd7412b5b984358e1fb9d38188935c3fb9c
DaichiSaito <h1a_ble@yahoo.co.jp>
2018-11-18 20:45:41 +0900

    be rails destroy model notification

--- app/models/notification.rb
+++
@@ -1,7 +0,0 @@
-class Notification < ApplicationRecord
-  belongs_to :notifiable, polymorphic: true
-  belongs_to :user
-
-  enum read: { unread: false, read: true }
-  scope :mokumoku_notifications, ->(mokumoku) { where(notifiable_type: 'Mokumoku').where(notifiable_id: mokumoku.id) }
-end
