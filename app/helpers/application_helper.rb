a4c47417920ed6a93624e030d48b4a517fc4cfa7
ArtefactAir <artefactapple@gmail.com>
2018-11-19 23:31:26 +0900

    Twitter シェア時の「〜から」の欄を削除 表示用途が不明なため

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
   def sns_share_url_with_message(message)
     "#{Settings.twitter.share_url}" \
     "?text=#{message}%0A" \
-    "&via=#{Settings.twitter.account_name}" \
     "&url=#{Settings.common.app.url}"
   end
 end
