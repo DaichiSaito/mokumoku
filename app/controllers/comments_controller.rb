81118c0309db41c6a43a81872a2cc826f7c8fb92
DaichiSaito <h1a_ble@yahoo.co.jp>
2018-11-25 12:29:04 +0900

    自分のコメントの場合でも自分に対して「あなたのもくもくにコメントがありました」と送られるバグ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,8 +21,12 @@ class CommentsController < ApplicationController
 
     def create_notifications
       return unless @comment.persisted?
-      @mokumoku.notifications.create(user_id: @mokumoku.user.id, notified_by: current_user, notified_type: :commented_to_own_mokumoku)
-      NotificationMailer.send_mokumoku_owner(@mokumoku.user, @mokumoku, @comment).deliver_later
+      # あなたのもくもくにコメントがありました。
+      # 自分のコメントの場合はこれは送らない
+      unless @mokumoku.user.id == current_user.id
+        @mokumoku.notifications.create(user_id: @mokumoku.user.id, notified_by: current_user, notified_type: :commented_to_own_mokumoku)
+        NotificationMailer.send_mokumoku_owner(@mokumoku.user, @mokumoku, @comment).deliver_later
+      end
       @mokumoku.participants.each do |user|
         next if user.id == current_user.id # 自分のコメントの場合自分には送らない
         @mokumoku.notifications.create(user_id: user.id, notified_by: current_user, notified_type: :commented_to_attending_mokumoku)
